ad28054705093818a20566e7465cba2e835fb006
mahmoud <mahmoudkhalefm9@gmial.com>
2024-04-23 12:03:37 -0700

    javascript

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+
+#!/usr/bin/node
+// Script that prints the title of a Star Wars movie where the episode number matches a given integer.
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], (err, response, body) => {
+  if (!err) {
+    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+      if (err) { console.error(err); }
+    });
+  }
+});
